fd9fe8c3112d832aa4a2c66e3f0cc2f9e17cc02e
Chattarika Bunchongpru <saiparn@Chattarikas-MacBook-Air.local>
2023-11-12 23:07:46 +0700

    features banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,12 @@ export default {
         '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       },
       dropShadow: {
+        //192,192,192,0.3  0, 0, 0, 0.25
         '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
         '4xl': ['0 35px 35px rgba(0, 0, 0, 0.25)', '0 45px 65px rgba(0, 0, 0, 0.15)'],
+      },
+      margin: {
+        '70px': '70px',
       },
     },
   },
